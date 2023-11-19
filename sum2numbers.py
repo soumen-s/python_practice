390f141809fca306e0073d903e7eae8b350b62a5
soumen-s <soumen1310.official@gmail.com>
2023-11-19 22:23:19 +0530

    adding 2 numbers taken from user

---
+++ sum2numbers.py
@@ -0,0 +1,10 @@
+#program to print the sum, where values are given by user 
+
+# typecsting the inputs while taking as it will detected as string by default
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+
+total = int(first)+int(second)  
+
+
+print("The sum is: ",total) 
