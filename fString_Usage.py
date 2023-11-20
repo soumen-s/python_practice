51e9257856a5d61d435ae5594c8c764a266342f4
soumen-s <soumen1310.official@gmail.com>
2023-11-20 23:01:55 +0530

    application of f-string in python

---
+++ fString_Usage.py
@@ -0,0 +1,10 @@
+# Program to see the usage of f-string
+# it is used as the alternative of '%d' in C language.
+
+country = "India"
+name = "Soumen"
+print(f"Hi! my name is {name} and i'm from {country}.")  #automatically applied
+
+price = 49.09999
+print(f"The price is: {price:.2f}") 
+# to get upto 2 floating point numbers (approximate also)
