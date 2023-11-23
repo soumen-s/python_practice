6cde9c040c5107a79cb3edc5c9edef668944e6f8
soumen-s <soumen1310.official@gmail.com>
2023-11-23 22:43:21 +0530

    List basics with initialization

---
+++ list_Basics.py
@@ -0,0 +1,17 @@
+'''
+ A list is a data structure in Python that 
+ is a mutable, or changeable, ordered sequence of elements.
+ It can hold elements of different datatypes.
+ Lists are defined by having values between square brackets [].
+'''
+
+integers = [11,12,13,14,"Soumen",True,15.3]
+
+# to get all elements
+print(integers) 
+
+# indicating one element through different ways
+print(integers[4])
+print(integers[len(integers)-3])
+print(integers[7-3])
+print(integers[-3]) # negetive index
