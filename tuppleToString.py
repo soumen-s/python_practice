93d16437d4e9296c564dcacde1dc435a28c58d31
soumen-s <soumen1310.official@gmail.com>
2023-12-19 23:48:32 +0530

    convet a tupple into string form

---
+++ tuppleToString.py
@@ -0,0 +1,8 @@
+# Converting a tupple into a string
+
+tup = ('S', 'o', 'u', 'm', 'e', 'n')
+
+# Use the 'join' method to concatenate the tupple elements
+str = ''.join(tup)
+
+print("The converted form of the tupple is:",str)
