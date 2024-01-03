f8359170cb7c46659b1530f68d44674154d9c350
soumen-s <soumen1310.official@gmail.com>
2024-01-03 23:58:52 +0530

    merging 2 lists to create dictionary

---
+++ merge2list-Dictionary.py
@@ -0,0 +1,8 @@
+# Program to merge 2 lists to create a dictionary dataset
+
+keys = ['name', 'age', 'city']
+values = ['Soumen', 21, 'Dubai :)']
+
+dictionary = dict(zip(keys, values))  #zipping into one
+
+print(dictionary)
